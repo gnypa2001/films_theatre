6b710e127cb46c6c8a2af9d4983c59760485e0a2
gnypa2001 <gnypa01@gmail.com>
2023-04-26 21:06:41 +0100

    added route for films page

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ app = Flask("Film theatre")
 @app.route('/')
 def main_page():
     return render_template("index.html")
+
+@app.route('/')
+def all_films():
+    return render_template("all_films.html")
+
 app.run(debug=True)

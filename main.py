1a67a2624838e70bb3296ae6879b7628e2a5cbb3
gnypa2001 <gnypa01@gmail.com>
2023-04-26 21:01:49 +0100

    added route for main page and changed buttons in index.html

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Film theatre")
 
+@app.route('/')
+def main_page():
+    return render_template("index.html")
 app.run(debug=True)

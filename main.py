547b11cc4fac6088d64ec60c27119c7938cbc7d7
gnypa2001 <gnypa01@gmail.com>
2023-05-01 14:10:10 +0100

    Added pages and styles, ps forgot to commit

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ app = Flask("Film theatre")
 def main_page():
     return render_template("index.html")
 
-@app.route('/')
+@app.route('/all_films')
 def all_films():
     return render_template("all_films.html")
+
+@app.route('/horror')
+def horrors():
+    return render_template("horror.html")
+
+@app.route('/comedy')
+def comedy():
+    return render_template("comedy.html")
 
 app.run(debug=True)

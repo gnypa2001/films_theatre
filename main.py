2300e4128bb4a88eed79f4b3dcad41c98f2c00d5
gnypa2001 <gnypa01@gmail.com>
2023-05-02 15:56:12 +0100

    created list with films

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template
 import json
 app = Flask("Film theatre")
-
+def get_spisok():
+    with open("films.json", "r") as file:
+        temp = json.load(file)
+    spisok = []
+    for name in temp:
+        spisok.append(name, temp[name]['descr'], temp[name]['link'], temp[name]['genre'])
+    return spisok
 @app.route('/')
 def main_page():
     return render_template("index.html")
 
 @app.route('/all_films')
 def all_films():
-   
-    return render_template("all_films.html")
+    return render_template("all_films.html", spisok = get_spisok())
 
 @app.route('/horror')
 def horrors():
-    return render_template("horror.html")
+    return render_template("horror.html", spisok = get_spisok())
 
 @app.route('/comedy')
 def comedy():
-    return render_template("comedy.html")
+    return render_template("comedy.html", spisok = get_spisok())
 
 @app.route('/action')
 def action():
-    return render_template("action.html")
+    return render_template("action.html", spisok = get_spisok())
 
 
 app.run(debug=True)
